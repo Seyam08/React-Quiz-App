f2bd5d7f190c4c838d79df7b575e417340fa60c2
Seyam <seyam.hossain.118833@gmail.com>
2023-05-12 23:26:05 +0600

    private route or outlet is working fine

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -2,6 +2,8 @@ import { Route, BrowserRouter as Router, Routes } from "react-router-dom";
 import { AuthProvider } from "../context/AuthContext";
 import "../styles/App.css";
 import Layout from "./Layout";
+import PrivateOutlet from "./PrivateOutlet";
+import PublicOutlet from "./PublicOutlet";
 import Home from "./pages/Home";
 import Login from "./pages/Login";
 import Quiz from "./pages/Quiz";
@@ -15,10 +17,14 @@ function App() {
         <Layout>
           <Routes>
             <Route exact path="/" element={<Home />} />
-            <Route exact path="/signup" element={<Signup />} />
-            <Route exact path="/login" element={<Login />} />
-            <Route exact path="/quiz" element={<Quiz />} />
-            <Route exact path="/result" element={<Result />} />
+            <Route path="/" element={<PublicOutlet />}>
+              <Route exact path="signup" element={<Signup />} />
+              <Route exact path="login" element={<Login />} />
+            </Route>
+            <Route path="/" element={<PrivateOutlet />}>
+              <Route exact path="quiz" element={<Quiz />} />
+              <Route exact path="result" element={<Result />} />
+            </Route>
           </Routes>
         </Layout>
       </AuthProvider>
